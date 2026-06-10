fbeffb828193cae223382ca6c17c69699def20a3
htsign <htsign@outlook.com>
2026-06-10 09:19:49 +0900

    modernize: type init.js and build `Wasavi` from typed constants

--- src/core/frontend/init.js
+++ src/core/frontend/init.js
@@ -33,35 +33,49 @@ const LOG_EX = false;
 const LOG_MAP_MANAGER = false;
 const LOG_LAST_SIMPLE_COMMAND = false;
 
-g.Wasavi = Object.defineProperties({}, {
-	IS_GECKO: {value: typeof g.browser !== 'undefined'
+/** @type {Pick<typeof Wasavi,
+ *   'IS_GECKO' | 'BRACKETS' | 'CLOSE_BRACKETS'
+ *   | 'LINE_NUMBER_MAX_WIDTH' | 'LINE_NUMBER_RELATIVE_WIDTH'
+ *   | 'COMPOSITION_CLASS' | 'LEADING_CLASS' | 'MARK_CLASS' | 'EMPHASIS_CLASS'
+ *   | 'CURSOR_SPAN_CLASS' | 'BOUND_CLASS'
+ *   | 'MIGEMO_EXTENSION_ID' | 'MIGEMO_GET_REGEXP_STRING'
+ *   | 'LOG_PROMISE' | 'LOG_EX' | 'LOG_MAP_MANAGER' | 'LOG_LAST_SIMPLE_COMMAND'>} */
+const constants = {
+	IS_GECKO: typeof g.browser !== 'undefined'
 		&& typeof g.browser.runtime !== 'undefined'
 		// Chrome 148+ also defines `browser` as an alias of `chrome`,
 		// so check the extension origin scheme to identify Firefox
-		&& g.browser.runtime.getURL('').startsWith('moz-extension://')},
+		&& g.browser.runtime.getURL('').startsWith('moz-extension://'),
 
-	BRACKETS: {value: BRACKETS},
-	CLOSE_BRACKETS: {value: CLOSE_BRACKETS},
+	BRACKETS,
+	CLOSE_BRACKETS,
 
-	LINE_NUMBER_MAX_WIDTH: {value: 6},
-	LINE_NUMBER_RELATIVE_WIDTH: {value: 2},
+	LINE_NUMBER_MAX_WIDTH: 6,
+	LINE_NUMBER_RELATIVE_WIDTH: 2,
 
-	COMPOSITION_CLASS: {value: 'wasavi_composition'},
-	LEADING_CLASS: {value: 'wasavi_leading'},
-	MARK_CLASS: {value: 'wasavi_mark'},
-	EMPHASIS_CLASS: {value: 'wasavi_em'},
-	CURSOR_SPAN_CLASS: {value: 'wasavi_command_cursor_span'},
-	BOUND_CLASS: {value: 'wasavi_bound'},
+	COMPOSITION_CLASS: 'wasavi_composition',
+	LEADING_CLASS: 'wasavi_leading',
+	MARK_CLASS: 'wasavi_mark',
+	EMPHASIS_CLASS: 'wasavi_em',
+	CURSOR_SPAN_CLASS: 'wasavi_command_cursor_span',
+	BOUND_CLASS: 'wasavi_bound',
 
-	MIGEMO_EXTENSION_ID: {value: 'dfccgbheolnlopfmahkcjiefggclmadb'},
-	MIGEMO_GET_REGEXP_STRING: {value: 'getRegExpString'},
+	MIGEMO_EXTENSION_ID: 'dfccgbheolnlopfmahkcjiefggclmadb',
+	MIGEMO_GET_REGEXP_STRING: 'getRegExpString',
 
-	LOG_PROMISE: {value: LOG_PROMISE},
-	LOG_EX: {value: LOG_EX},
-	LOG_MAP_MANAGER: {value: LOG_MAP_MANAGER},
-	LOG_LAST_SIMPLE_COMMAND: {value: LOG_LAST_SIMPLE_COMMAND}
-});
+	LOG_PROMISE,
+	LOG_EX,
+	LOG_MAP_MANAGER,
+	LOG_LAST_SIMPLE_COMMAND
+};
 
-})(typeof global == 'object' ? global : window);
+// Wasavi is assembled across files: init.js defines the constants below, while
+// each class file appends its own constructor. Lock the constants (non-writable,
+// non-enumerable, non-configurable) as `Object.defineProperties` did originally.
+g.Wasavi = /** @type {typeof Wasavi} */ ({});
+Object.entries(constants).forEach(([key, value]) =>
+	Object.defineProperty(g.Wasavi, key, {value}));
+
+})(typeof globalThis == 'object' ? globalThis : window);
 
 // vim:set ts=4 sw=4 fenc=UTF-8 ff=unix ft=javascript fdm=marker :
